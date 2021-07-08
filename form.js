a8a585fb7286206bd455f87b6a3f38a84d59b7a8
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-07-08 16:56:04 +1000

    Updates to functionality and styles

--- form.js
+++ form.js
@@ -1,23 +1,52 @@
 const form = document.querySelector('form')
-const submit = document.querySelector('.form-btn')
+const surname = document.querySelector('#surname')
+const firstname = document.querySelector('#firstname')
+const telephone = document.querySelector('#telephone')
+const email = document.querySelector('#email')
+const message = document.querySelector('#message')
+const span = document.querySelectorAll('.form-required')
 
+//If JS has loaded the HTML validations are removed
+form.setAttribute('novalidate', true)
+
+//Validation regexs
+const nameReg = /^[A-Za-zÀ-ÖØ-öø-ÿ \-'.0-9]+$/
 const emailReg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+const phoneReg = /^[^a-z]([\d]{1,})[^a-z]$/
+const messageReg = /^[^<][\s\S]+$/
 
-//remove require attribute
+//Variable to test if validation tests are passed and an array of error messages
+let tests = false
+const errorMsg = ["This field is required", "This information is invalid - please update"]
 
-submit.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(emailReg.test(form[3].value))
-    validateForm(form)
-    // displayForm()
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //Remove any previous errors from the span during form resubmission
+    for(let spans of span) { 
+        spans.innerText = "" 
+    } 
+    //Perform validations on each input
+    validateForm(nameReg, surname) &&
+    validateForm(nameReg, firstname) &&
+    validateForm(phoneReg, telephone) &&
+    validateForm(emailReg, email) &&
+    validateForm(messageReg, message) ? confirm() : null
 })
 
-const validateForm = (values) => {
-    emailReg.test(values[3].value) ? null : values[3].setAttribute('placeholder', 'Please enter valid email')  
+//If an input has no value an error is issued and if the information doesn't pass validation an error is issued about the input area
+function validateForm(regex, input) {
+    if(input.value === "" && input.id !== "telephone"){
+        input.setAttribute('placeholder', errorMsg[0])
+        return tests = false
+    } else if(!regex.test(input.value) && input.value !== ""){
+        input.parentNode.childNodes[1].childNodes[1].innerText = errorMsg[1]
+        return tests = false
+    } else {
+        return tests = true
+    }
 }
 
-const displayForm = () => {
-    let phoneEntry
-    form[2].value === "" ? phoneEntry = null : phoneEntry = form[2].value
-    console.log(`name: ${form[1].value} ${form[0].value}\nphone: ${phoneEntry}\nemail: ${form[3].value}\nmessage: ${form[4].value}`) 
+//If all inputs are valid the 'tests' variable is set to true and the next function is called
+const confirm = () => {
+    tests ? console.log(`Name: ${firstname.value} ${surname.value}\nTelephone: ${telephone.value}\nE-Mail: ${email.value}\nMessage: ${message.value}`) : null
 }
